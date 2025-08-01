2fe1d0c172da2d30e9f6f88cd82d21b1e369b623
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-02 01:03:07 +0530

    09-RESTController

---
+++ 09_RESTController/src/main/java/lk/ijse/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse;
+
+import lk.ijse.config.WebAppConfig;
+import lk.ijse.config.WebRootConfig;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        //business logics(parent context)
+        return new Class<?>[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        //request mapping,controllers,view controllers(child context)
+        return new Class<?>[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
